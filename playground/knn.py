c5712108bad9b6bbc26921e54a82e8abf5ad0f13
Nicole <nicole.l.suet@gmail.com>
2024-07-23 19:37:21 -0300

    knn

---
+++ playground/knn.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+dataset = pd.read_csv(url, names=names)
+
+dataset.head()
+
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 4].values
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, Y_train)
+
+y_pred = classifier.predict(X_test)
+report = classification_report(Y_test, y_pred)
+matrix = confusion_matrix(Y_test, y_pred)
+
+print(report)
+print(matrix)
